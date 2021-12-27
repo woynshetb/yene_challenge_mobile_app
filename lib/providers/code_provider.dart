fe5536b61d0f96b4912b469c38ff947fb389c087
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-27 16:52:19 +0300

    comment out unused functions

--- lib/providers/code_provider.dart
+++ lib/providers/code_provider.dart
@@ -5,7 +5,7 @@ import '../models/code.dart';
 import 'package:http/http.dart' as http;
 
 class CodeProvider with ChangeNotifier {
-  List<Code> codeInfo = [Code("", 0)];
+  //List<Code> codeInfo = [Code("", 0)];
   List<User> user = [User(0)];
 
   incrementpoint(point) {
@@ -21,35 +21,33 @@ class CodeProvider with ChangeNotifier {
 
     if (response.statusCode == 200) {
       incrementpoint(point);
-      //await getUpdatedFile(code);
-    } else {
-      print(response.statusCode);
-    }
-  }
-
-  Future getUpdatedFile(String code) async {
-    var url = Uri.parse("https://yeneproject.herokuapp.com/getcodepoint/$code");
-    var response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      debugPrint(response.body);
-
-      var decode = jsonDecode(response.body);
-      codeInfo.insert(0, Code(decode['code'], decode['points']));
-      //codeInfo.add(Code(decode['code'], decode['points']));
     } else {
       debugPrint(response.statusCode.toString());
     }
   }
+
+  // Future getUpdatedFile(String code) async {
+  //   var url = Uri.parse("https://yeneproject.herokuapp.com/getcodepoint/$code");
+  //   var response = await http.get(url);
+
+  //   if (response.statusCode == 200) {
+  //     debugPrint(response.body);
+
+  //     var decode = jsonDecode(response.body);
+  //     codeInfo.insert(0, Code(decode['code'], decode['points']));
+  //   } else {
+  //     debugPrint(response.statusCode.toString());
+  //   }
+  // }
 
   Future generateCodeAndTransferCode(int points) async {
     var url =
         Uri.parse("https://yeneproject.herokuapp.com/generatecodefromUser");
     var response = await http.post(url, body: {"points": "$points"});
     if (response.statusCode == 200) {
-      print(response.body);
+      debugPrint(response.body);
     } else {
-      print(response.statusCode);
+      debugPrint(response.statusCode.toString());
     }
   }
 }
